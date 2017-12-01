0787e4ebad170fee2294efc04001b4118538ec98
Xavier <xpfplantaz@connect.ust.hk>
2017-12-02 04:13:41 +0800

    Update Mingin UI

--- src/components/mining.js
+++ src/components/mining.js
@@ -1,67 +1,39 @@
 import React, { Component } from 'react';
-
-import {polarToCartesian} from '../helpers/arcs';
-import {describeArc} from '../helpers/arcs';
-
-import circleImage from '../images/circles.png';
-import circlesMiningData from '../api/circlesMining'
+import CircleDesign from './miningElements/circlesDesign'
+import './mining.css';
+import circlesMiningData from '../api/circlesMining';
 
 class Mining extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {clickTotal: 0};
+    this.circlesMiningData = circlesMiningData;
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log(this.clickTotal)
+    this.setState(prevState => ({
+      clickTotal: prevState.clickTotal += 1
+    }));
+    circlesMiningData['circles'][0].numberOfElementsMissing-=1;
+    if(circlesMiningData['circles'][0].numberOfElementsMissing<0){
+        circlesMiningData['circles'][0].numberOfElementsMissing=circlesMiningData['circles'][0].numberOfElements;
+    }
+  }
+
   render() {
-    let circleBackground = {
-        position:"absolute",
-        top:26,
-        opacity:0.2,
-        zIndex:-99
-    };
-
-    console.log(circlesMiningData);
-    let circlesElement = circlesMiningData['circles'].map((circle,index)=>{
-        let n = circle.numberOfElements;
-        n -= circle.numberOfElementsMissing?circle.numberOfElementsMissing:0;
-        return (
-            <g key={index}>
-                {
-                    [...Array(n)].map((d,p)=>
-                        <path id={circle.id+'-'+p} 
-                            key={circle.id+'-'+p} 
-                            fill={circle.fill} 
-                            stroke={circle.stroke} 
-                            strokeWidth={circle.strokeWidth} 
-                            opacity={circle.opacity} 
-                            d={describeArc( 
-                                200,
-                                200,
-                                circle.r,
-                                circle.startAngle + p*360/circle.numberOfElements,
-                                circle.startAngle + p*360/circle.numberOfElements + (360/circle.numberOfElements-circle.spaceBetweenElements))}
-                        >
-                        </path>
-                    )
-                }
-            {circle.rotationSpeed &&
-                <animateTransform attributeType="xml"
-                    attributeName="transform"
-                    type="rotate"
-                        from="0 200 200" 
-                        to={circle.rotationDirection == -1?"-360 200 200":"360 200 200"}
-                    dur={circle.rotationSpeed}
-                    repeatCount="indefinite"/>
-            }
-            </g>
-        )
-    })
-
     return (
         <div>
-            <p>Hello!</p>
-            <img src={circleImage} alt="Background Circles" style={circleBackground}/>
-            <svg width="400px" height="400px">
+            <h1>Mining</h1>
+            <svg onClick={this.handleClick} width="400px" height="400px">
                  <filter id="blurMe">
                     <feGaussianBlur in="SourceGraphic" stdDeviation="5"/>
                 </filter>
-                <g filter="url(#blurMe)">{circlesElement}</g>
-                <g>{circlesElement}</g>
+                <g filter="url(#blurMe)"><CircleDesign/></g>
+                <g><CircleDesign circlesMiningData={circlesMiningData}/></g>
             </svg>
         </div>
     );
---
+++ src/components/miningElements/circlesDesign.js
@@ -0,0 +1,55 @@
+
+import React, { Component } from 'react';
+
+import {describeArc} from '../../helpers/arcs';
+
+
+class CircleDesign extends Component {
+  render() {
+
+    let circlesElement = '';
+    if(this.props.circlesMiningData){
+        circlesElement = this.props.circlesMiningData['circles'].map((circle,index)=>{
+        let n = circle.numberOfElements;
+        n -= circle.numberOfElementsMissing?circle.numberOfElementsMissing:0;
+        return (
+            <g key={index}>
+                {
+                    [...Array(n)].map((d,p)=>
+                        <path id={circle.id+'-'+p} 
+                            key={circle.id+'-'+p} 
+                            fill={circle.fill} 
+                            stroke={circle.stroke} 
+                            strokeWidth={circle.strokeWidth} 
+                            opacity={circle.opacity} 
+                            d={describeArc( 
+                                200,
+                                200,
+                                circle.r,
+                                circle.startAngle + p*360/circle.numberOfElements,
+                                circle.startAngle + p*360/circle.numberOfElements + (360/circle.numberOfElements-circle.spaceBetweenElements))}
+                        >
+                        </path>
+                    )
+                }
+            {circle.rotationSpeed &&
+                <animateTransform attributeType="xml"
+                    attributeName="transform"
+                    type="rotate"
+                        from="0 200 200" 
+                        to={circle.rotationDirection == -1?"-360 200 200":"360 200 200"}
+                    dur={circle.rotationSpeed}
+                    repeatCount="indefinite"/>
+            }
+            </g>
+            )
+        })
+    }
+
+    return (
+        <g>{circlesElement}</g>
+    );
+  }
+}
+
+export default CircleDesign;
