f4ca5dd107e80152f0342b6c6e977541a1612d49
Xavier <xpfplantaz@connect.ust.hk>
2017-12-02 04:14:17 +0800

    Update Network Layout

---
+++ src/api/circlesMining.js
@@ -0,0 +1,137 @@
+module.exports = {
+	circles: 
+	[
+		{
+			id: 'arc1', 
+			fill: 'none', 
+            stroke: '#446688',
+            strokeWidth: 14,
+            r: 150,
+            startAngle: 227,
+            numberOfElements: 24,
+			spaceBetweenElements: 5,
+			numberOfElementsMissing:6
+            
+		},
+		{
+			id: 'arc2', 
+			fill: 'none', 
+            stroke: '#53e1f2',
+            strokeWidth: 3,
+            r: 132,
+            startAngle: 10,
+            numberOfElements: 10,
+			spaceBetweenElements: 8,
+			rotationDirection: 1,
+			rotationSpeed: '60s'
+		},
+		{
+			id: 'arc3', 
+			fill: 'none', 
+            stroke: '#446688',
+            strokeWidth: 24,
+            r: 110,
+            startAngle: 20,
+            numberOfElements: 2,
+			spaceBetweenElements: 90,
+			opacity: 0.5
+            
+		},
+		{
+			id: 'arc4', 
+			fill: 'none', 
+            stroke: '#53e1f2',
+            strokeWidth: 24,
+            r: 110,
+            startAngle: 40,
+            numberOfElements: 2,
+			spaceBetweenElements: 90,
+			opacity: 0.5
+            
+		},
+		{
+			id: 'arc5', 
+			fill: 'none', 
+            stroke: '#446688',
+            strokeWidth: 2,
+            r: 92,
+            startAngle: 0,
+            numberOfElements: 20,
+			spaceBetweenElements: -2,
+			opacity: 0.2
+		},
+
+		{
+			id: 'arc6', 
+			fill: 'none', 
+            stroke: '#53e1f2',
+            strokeWidth: 3,
+            r: 83,
+            startAngle: 20,
+            numberOfElements: 5,
+			spaceBetweenElements: 20,
+			opacity: 1,
+			rotationDirection: 1,
+			rotationSpeed: '10s'
+		},
+		{
+			id: 'arc7', 
+			fill: 'none', 
+            stroke: '#53e1f2',
+            strokeWidth: 2,
+            r: 72,
+            startAngle: 0,
+            numberOfElements: 20,
+			spaceBetweenElements: -2,
+			opacity: 0.2
+		},
+		{
+			id: 'arc8', 
+			fill: 'none', 
+            stroke: '#446688',
+            strokeWidth: 10,
+            r: 59,
+            startAngle: 180,
+            numberOfElements: 24,
+			spaceBetweenElements: 5,
+			numberOfElementsMissing:14
+            
+		},
+		{
+			id: 'arc9', 
+			fill: 'none', 
+            stroke: '#53e1f2',
+            strokeWidth: 3,
+            r: 45,
+            startAngle: 0,
+            numberOfElements: 6,
+			spaceBetweenElements: 20,
+			rotationDirection: -1,
+			rotationSpeed: '10s'
+		},
+		{
+			id: 'arc9', 
+			fill: 'none', 
+            stroke: '#53e1f2',
+            strokeWidth: 3,
+            r: 35,
+            startAngle: 90,
+            numberOfElements: 2,
+			spaceBetweenElements: 120,
+			rotationSpeed:'2s'
+		},
+		{
+			id: 'arc7', 
+			fill: 'none', 
+            stroke: '#446688',
+            strokeWidth: 2,
+            r: 27,
+            startAngle: 0,
+            numberOfElements: 20,
+			spaceBetweenElements: -2,
+			opacity: 0.2,
+			rotationSpeed:'10s'
+		},
+	]
+};
+
---
+++ src/components/network.js
@@ -0,0 +1,47 @@
+import './network.css';
+import React, { Component } from 'react';
+import { geoMercator, geoPath } from 'd3-geo'
+import worlddata from '../static/world'
+import citiesdata from '../static/cities'
+
+class Network extends Component {
+  render() {
+    const projection = geoMercator()
+    const pathGenerator = geoPath().projection(projection)
+    const countries = worlddata.features
+      .map((d,i) => <path
+      key={'path' + i}
+      d={pathGenerator(d)}
+      className='countries'
+/>)
+
+    console.log(citiesdata.cities)
+    const cities = citiesdata.cities
+      .map((d,i) => <circle
+      key={'city-' + i}
+      cx={projection([d.lng,d.lat])[0]}
+      cy={projection([d.lng,d.lat])[1]}
+      r="2" 
+      className='city'
+      />)
+    
+    return (
+      <div className="network">
+        <h1>Network</h1>
+        <svg width="100%" height="400">
+          <filter id="blurMe3">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="5"/>
+          </filter>
+          <g filter="url(#blurMe3)">{countries}</g>
+          <g>{countries}</g>
+
+          <g filter="url(#blurMe3)">{cities}</g>
+          <g>{cities}</g>
+
+        </svg>
+      </div>
+    )
+  }
+}
+
+export default Network;
